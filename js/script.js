074bac6a9801ce517d5569a4a4a2152c10cb6e34
manish mandal <76776194mm@gmail.com>
2022-10-04 19:35:41 +0530

    update js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+'use strict'
+ 
+const firstName=document.querySelector('#name');
+const errorName=document.querySelector('#error-name');
+
+const cardNumber=document.querySelector('#card-number');
+const errorCardNumber=document.querySelector('#error-card-no');
+
+const month=document.querySelector('#month');
+const monthError=document.querySelector('.error-month');
