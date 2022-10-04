618f680004b12e8549f2d0e6d4719d929efef6ae
manish mandal <76776194mm@gmail.com>
2022-10-04 22:11:19 +0530

    update js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
 'use strict'
  
-const firstName=document.querySelector('#name');
-const errorName=document.querySelector('#error-name');
+const Name=document.querySelector('#name');
+const errorName=document.querySelector('.error-name');
 
 const cardNumber=document.querySelector('#card-number');
-const errorCardNumber=document.querySelector('#error-card-no');
+const errorCardNumber=document.querySelector('.error-card-no');
 
 const month=document.querySelector('#month');
 const monthError=document.querySelector('.error-month');
@@ -17,3 +17,22 @@ const errorCVC=document.querySelector('.error-cvc');
 
 const formFillUp=document.querySelector('.form-fill-up');
 const cradCompleteState=document.querySelector('.card-complete-state');
+
+let nameUpdate;
+Name.addEventListener('keyup',function(){
+  const nameValue=Name.value;
+  if(/\d/.test(nameValue)){ 
+    
+    errorName.textContent="Invalid input";
+    nameUpdate=nameValue.slice(0,nameValue.length-1);
+    Name.value=nameUpdate;
+    console.log(2);
+  }
+  else if(nameValue === ''){
+    errorName.textContent="Name cannot be empty"; 
+  }
+  else{
+    nameUpdate=nameValue;
+    errorName.textContent="";
+  }
+});
